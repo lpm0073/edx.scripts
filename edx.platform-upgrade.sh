9e4f74d4d6cb54f66463ef009781690a08b4d582
Lawrence McDaniel <lpm0073@gmail.com>
2018-04-09 10:43:33 -0500

    documentation

--- edx.platform-upgrade.sh
+++ edx.platform-upgrade.sh
@@ -7,12 +7,25 @@
 #            This script takes about an hour to run. I STRONGLY recommend that you monitor
 #            its progress as it runs.
 #
-# CRITICALLY IMPORTANT NOTE:
+#            When attemping Open edX upgrades I've often struggled with subsystem version nuances.
+#            The most problematic in my experience are Python pip packages, and pip itself. To
+#            attempt to mitigate this problem I prefer to completely delete the existing virtual
+#            environments for both Python and Node. The upgrade scripts will automatically rebuild
+#            these virtual environments for you using the current recommended versions of each package.
+#
+# CRITICALLY IMPORTANT NOTE 1:
 #           before attempting this script you should completely backup your server
 #           by creating an image of your current EC2 instance on AWS.
 #
+# CRITICALLY IMPORTANT NOTE 2:
+#           This script creates a backup of your currently-installed Open edX code base.
+#           This is a large code base in excess of 5Gb. You should verify that the disk volume
+#           on your EC2 Ubuntu instance has sufficient available space before attempting to run
+#           this script!
+#
 # Reference: https://openedx.atlassian.net/wiki/spaces/OpenOPS/pages/60227913/Managing+OpenEdX+Tips+and+Tricks
 #---------------------------------------------------------
+NOW="$(date +%Y%m%dT%H%M%S)"
 
 # Upgrade to the most recent code base. thus far i've found this more reliable than named releases.
 export OPENEDX_RELEASE=master
@@ -34,36 +47,25 @@ sudo /edx/bin/supervisorctl stop xqueue
 sudo /edx/bin/supervisorctl stop xqueue_consumer
 
 # Step 2: backup the existing environment.
-  #Check to see if a backup of nodeenvs exists. if so, delete it.
-  if [ -d /edx/app/edxapp/edx-platform-BACKUP/ ]; then
-    sudo rm -r /edx/app/edxapp/edx-platform-BACKUP/
-    echo "found existing backup of /edx/app/edxapp/edx-platform folder. removing"
-  fi
 
-  #Check to see if a backup of edxapp_env exists. if so, delete it.
-  if [ -d /edx/app/edxapp/edxapp_env-BACKUP/ ]; then
-    sudo rm -r /edx/app/edxapp/edxapp_env-BACKUP/
-    echo "found existing backup of /edx/app/edxapp/edxapp_env folder. removing"
-  fi
+echo "Backing up /edx/app/edxapp/edx-platform/"
+if [ -d /edx/app/edxapp/edx-platform/ ]; then
+  sudo mv /edx/app/edxapp/edx-platform/ /edx/app/edxapp/edx-platform-BACKUP-${NOW}
+fi
 
-  #Check to see if a backup of edxapp_env exists. if so, delete it.
-  if [ -d /edx/app/edxapp/venvs-BACKUP/ ]; then
-    sudo rm -r /edx/app/edxapp/venvs-BACKUP/
-    echo "found existing backup of /edx/app/edxapp/venvs folder. removing"
-  fi
+if [ -d /edx/app/edxapp/edxapp_env/ ]; then
+  sudo mv /edx/app/edxapp/edxapp_env/ /edx/app/edxapp/edxapp_env-BACKUP-${NOW}
+fi
 
-  #Check to see if a backup of nodeenvs exists. if so, delete it.
-  if [ -d /edx/app/edxapp/nodeenvs-BACKUP/ ]; then
-    sudo rm -r /edx/app/edxapp/nodeenvs-BACKUP/
-    echo "found existing backup of /edx/app/edxapp/nodeenvs folder. removing"
-  fi
+if [ -d /edx/app/edxapp/venvs/ ]; then
+  sudo mv /edx/app/edxapp/venvs/ /edx/app/edxapp/venvs-BACKUP-${NOW}
+fi
 
-  #Backup current Installation
-  echo "Backing up /edx/app/edxapp/edx-platform/"
-  sudo mv /edx/app/edxapp/edx-platform/ /edx/app/edxapp/edx-platform-BACKUP
-  sudo mv /edx/app/edxapp/edxapp_env/ /edx/app/edxapp/edxapp_env-BACKUP
-  sudo mv /edx/app/edxapp/venvs/ /edx/app/edxapp/venvs-BACKUP
-  sudo mv /edx/app/edxapp/nodeenvs/ /edx/app/edxapp/nodeenvs-BACKUP
+if [ -d /edx/app/edxapp/nodeenvs/ ]; then
+  sudo mv /edx/app/edxapp/nodeenvs/ /edx/app/edxapp/nodeenvs-BACKUP-${NOW}
+fi
+
+
 
 # Step 3: Perform the upgrade
 sudo /edx/bin/update edx-platform ${OPENEDX_RELEASE}
