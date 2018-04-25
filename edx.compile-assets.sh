6ae9a90122f06368772db82cf9e5c752094f77a7
Lawrence McDaniel <lpm0073@gmail.com>
2018-04-25 14:13:59 -0500

    add CMS

--- edx.compile-assets.sh
+++ edx.compile-assets.sh
@@ -26,6 +26,7 @@ sudo -H -u edxapp bash << EOF
 source /edx/app/edxapp/edxapp_env
 cd /edx/app/edxapp/edx-platform
 paver update_assets lms --settings=aws
+paver update_assets cms --settings=aws
 EOF
 
 # restart edx instances
