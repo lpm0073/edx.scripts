7b36885f1e05f7aafcef5413182262f1b46a23e9
Lawrence McDaniel <lpm0073@gmail.com>
2021-01-11 11:18:40 -0600

    change edx-platform fork to open-release/koa.master

--- edx.platform-install.sh
+++ edx.platform-install.sh
@@ -41,7 +41,7 @@ export LC_CTYPE="en_US.UTF-8"
 #export OPENEDX_RELEASE=open-release/ginkgo.2
 # Note: sometimes there are important bug fixes in master that are not included in the named releases.
 #       to date i've always had the best luck with master.
-export OPENEDX_RELEASE=open-release/juniper.master
+export OPENEDX_RELEASE=open-release/koa.master
 
 # 2. Bootstrap the Ansible installation:
 wget https://raw.githubusercontent.com/edx/configuration/$OPENEDX_RELEASE/util/install/ansible-bootstrap.sh -O - | sudo -H bash
