a8823796d0eae10b6d065d72d6d2a870f93ef0b1
Lawrence McDaniel <lpm0073@gmail.com>
2021-02-09 11:49:29 -0600

    ansible-bootstrap.sh -O - | sudo -E bash

--- edx.platform-install.sh
+++ edx.platform-install.sh
@@ -42,7 +42,7 @@ export LC_CTYPE="en_US.UTF-8"
 export OPENEDX_RELEASE=open-release/koa.master
 
 # 2. Bootstrap the Ansible installation:
-wget https://raw.githubusercontent.com/edx/configuration/$OPENEDX_RELEASE/util/install/ansible-bootstrap.sh -O - | sudo -H bash
+wget https://raw.githubusercontent.com/edx/configuration/$OPENEDX_RELEASE/util/install/ansible-bootstrap.sh -O - | sudo -E bash
 
 # 3. (Optional) If this is a new installation, randomize the passwords:
 wget https://raw.githubusercontent.com/edx/configuration/$OPENEDX_RELEASE/util/install/generate-passwords.sh -O - | bash
