4c3aa07706282a7bc9db5c65d780e6732f7dbb21
Lawrence McDaniel <lpm0073@gmail.com>
2021-02-27 09:26:41 -0600

    modify settings parameter to production

--- edx.platform-migrations.sh
+++ edx.platform-migrations.sh
@@ -8,12 +8,21 @@
 # Reference: https://openedx.atlassian.net/wiki/spaces/OpenOPS/pages/60227913/Managing+OpenEdX+Tips+and+Tricks
 #---------------------------------------------------------
 
+echo 'edx.platform-migrations.sh'
+
+# Sanity check: is there an Open edX platform on this server?
+if [ ! -d "/edx/app/edxapp/edx-platform/" ]; then
+  echo "Didn't find an Open edX platform on this server. Exiting"
+  exit
+fi
+
 sudo -H -u edxapp -s bash << EOF
 cd ~
 source /edx/app/edxapp/edxapp_env
 
-python /edx/app/edxapp/edx-platform/manage.py lms makemigrations --settings=aws
-python /edx/app/edxapp/edx-platform/manage.py lms migrate --settings=aws
-python /edx/app/edxapp/edx-platform/manage.py cms makemigrations --settings=aws
-python /edx/app/edxapp/edx-platform/manage.py cms migrate --settings=aws
+python /edx/app/edxapp/edx-platform/manage.py lms makemigrations --settings=production
+python /edx/app/edxapp/edx-platform/manage.py lms migrate --settings=production
+
+python /edx/app/edxapp/edx-platform/manage.py cms makemigrations --settings=production
+python /edx/app/edxapp/edx-platform/manage.py cms migrate --settings=production
 EOF
