0836b20f4ac8517e7d3f9b1f0d5ff160918a7c94
Lawrence McDaniel <lpm0073@gmail.com>
2022-01-12 17:27:36 -0600

    change github organization from edx to openedx

--- edx.platform-install.sh
+++ edx.platform-install.sh
@@ -42,10 +42,10 @@ export LC_CTYPE="en_US.UTF-8"
 export OPENEDX_RELEASE=open-release/lilac.master
 
 # 2. Bootstrap the Ansible installation:
-wget https://raw.githubusercontent.com/edx/configuration/$OPENEDX_RELEASE/util/install/ansible-bootstrap.sh -O - | sudo -E bash
+wget https://raw.githubusercontent.com/openedx/configuration/$OPENEDX_RELEASE/util/install/ansible-bootstrap.sh -O - | sudo -E bash
 
 # 3. (Optional) If this is a new installation, randomize the passwords:
-wget https://raw.githubusercontent.com/edx/configuration/$OPENEDX_RELEASE/util/install/generate-passwords.sh -O - | bash
+wget https://raw.githubusercontent.com/openedx/configuration/$OPENEDX_RELEASE/util/install/generate-passwords.sh -O - | bash
 
 # 4. Install Open edX:
-wget https://raw.githubusercontent.com/edx/configuration/$OPENEDX_RELEASE/util/install/native.sh -O - | bash > install.out
+wget https://raw.githubusercontent.com/openedx/configuration/$OPENEDX_RELEASE/util/install/native.sh -O - | bash > install.out
